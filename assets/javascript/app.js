2e108a101586e9592151c0d7a12b43d58fbb4400
Ryan Rodwell <ryanrodwell@lawn-128-61-49-109.lawn.gatech.edu>
2017-03-21 18:50:21 -0400

    fixing sign in client ID and adding sign out link

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -241,3 +241,9 @@ console.log("hi!");
       'onfailure': onFailure
     });
   }
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
