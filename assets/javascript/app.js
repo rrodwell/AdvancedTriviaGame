8142035dc1a7a16140652f5470df65cceda6ef38
Ryan Rodwell <ryanrodwell@lawn-128-61-49-109.lawn.gatech.edu>
2017-03-21 19:13:59 -0400

    fixing log out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -222,6 +222,7 @@ console.log("hi!");
    console.log('Name: ' + profile.getName());
    console.log('Image URL: ' + profile.getImageUrl());
    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+   gapi.auth2.init();
  }
 
  function onSuccess(googleUser) {
